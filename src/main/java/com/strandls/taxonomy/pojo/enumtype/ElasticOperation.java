2ae3bb0d6ec6bb129c20070bdc14b78bd3cfdf71
vilay <kandi.vilay09@gmail.com>
2021-05-08 10:06:29 +0530

    Fix : code refactor

--- src/main/java/com/strandls/taxonomy/pojo/enumtype/ElasticOperation.java
+++ src/main/java/com/strandls/taxonomy/pojo/enumtype/ElasticOperation.java
@@ -1,29 +1,6 @@
 package com.strandls.taxonomy.pojo.enumtype;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlEnumValue;
-import javax.xml.bind.annotation.XmlType;
-
-@XmlType(name = "elasticOperation")
-@XmlEnum
 public enum ElasticOperation {
-
-	@XmlEnumValue("CREATE")
-	CREATE("CREATE"),
-	@XmlEnumValue("UPDATE")
-	UPDATE("UPDATE");
-
-	private String value;
-	
-	private ElasticOperation(String value) {
-		this.value = value;
-	}
-	
-	public static ElasticOperation fromValue(String value) {
-		for(ElasticOperation layerStatus : ElasticOperation.values()) {
-			if(layerStatus.value.equals(value))
-				return layerStatus;
-		}
-		throw new IllegalArgumentException(value);
-	}
+	CREATE,
+	UPDATE;
 }
